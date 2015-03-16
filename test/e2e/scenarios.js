246be8d2b9723728d4694c2314350030158952d7
Sean Chen <sean.chen@leocorn.com>
2015-03-16 10:16:14 -0400

    try the e2e testing by using Protractor.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,22 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
+// describe is the syntax from Jasmine framework,
 describe('my app', function() {
 
   beforeEach(function() {
-    browser.get('app/index.html');
+    // browser is a global created by Protractor.
+    browser.get('index.html');
   });
 
+  /* testing some content of the page */
+  // it is the syntax from Jasmine framework.
+  it("try to get some content of the homepage", function() {
+    // expect title to be...
+    expect(browser.getTitle()).toMatch("Angular Trac Client");
+
+    // now let's find the first p tag.
+    expect(element(by.tagName('p')).getText()).
+      toMatch("Nothing here yet!");
+  });
 });

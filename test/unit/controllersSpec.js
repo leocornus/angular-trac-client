c4a58b95efa04eee3415e716a6f6330ae48777a4
Sean Chen <sean.chen@leocorn.com>
2015-03-17 10:51:54 -0400

    here are more unit testing.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -14,10 +14,21 @@ describe('angular trac client controllers', function() {
     it('should create "tickets" model with 2 tickets', 
        inject(function($controller) {
 
-      var scope = {};
-      var ctrl = $controller('TracHomeCtrl', {$scope:scope});
+      var ticketsScope = {};
+      var ctrl = $controller('TracHomeCtrl', 
+                             {$scope:ticketsScope});
+      // we have 2 dummy tickets for testing.
+      expect(ticketsScope.tickets.length).toBe(2);
 
-      expect(scope.tickets.length).toBe(2);
+      for(var i=0; i < ticketsScope.tickets.length; i++) {
+        var ticket = ticketsScope.tickets[i];
+        // each ticket is a JavaScript Object, iand it has 2 fields.
+        var keys = Object.keys(ticket);
+        expect(keys.length).toBe(2);
+        // we should know the keys.
+        expect(keys[0]).toMatch('id');
+        expect(keys[1]).toMatch('summary');
+      }
     }));
   });
 });

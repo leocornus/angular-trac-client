55a916b6079d809b48452dc9f3ae1afdad8a5d42
Sean Chen <sean.chen@leocorn.com>
2015-04-22 14:57:35 -0400

    adding the screenshot reporter.

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,3 +1,5 @@
+var HtmlReporter = require('protractor-html-screenshot-reporter');
+
 exports.config = {
   allScriptsTimeout: 11000,
 
@@ -14,6 +16,12 @@ exports.config = {
   baseUrl: 'http://localhost:8800/app/',
 
   framework: 'jasmine',
+
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: 'screenshots'
+    }));
+  },
 
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000

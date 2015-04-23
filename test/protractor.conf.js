5196585697fe5ad66460dc38054df3c78fc7b1af
Sean Chen <sean.chen@leocorn.com>
2015-04-23 11:20:20 -0400

    creae a path builder for the html screenshot reporter

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,4 +1,5 @@
 var HtmlReporter = require('protractor-html-screenshot-reporter');
+var path = require('path');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -19,11 +20,38 @@ exports.config = {
 
   onPrepare: function() {
     jasmine.getEnv().addReporter(new HtmlReporter({
-      baseDirectory: 'screenshots'
+      baseDirectory: 'screenshots',
+      takeScreenShotsOnlyForFailedSpecs: false,
+      pathBuilder: function pathBuilder(spec, descriptions, 
+                                        results, capabilities) {
+        var monthMap = {
+          "1": "Jan",
+          "2": "Feb",
+          "3": "Mar",
+          "4": "Apr",
+          "5": "May",
+          "6": "Jun",
+          "7": "Jul",
+          "8": "Aug",
+          "9": "Sep",
+          "10": "Oct",
+          "11": "Nov",
+          "12": "Dec"
+        };
+
+        var currentDate = new Date(),
+            currentHoursIn24Hour = currentDate.getHours(),
+            currentTimeInHours = currentHoursIn24Hour>12 ? currentHoursIn24Hour - 12 : currentHoursIn24Hour,
+            totalDateString = currentDate.getDate()+'-'+ monthMap[currentDate.getMonth()+1]+ '-'+(currentDate.getYear()+1900) + 
+                                '-'+ currentTimeInHours+'h-' + currentDate.getMinutes()+'m';
+
+        return path.join(totalDateString,capabilities.caps_.browserName, descriptions.join('-'));
+      }
     }));
   },
 
   jasmineNodeOpts: {
+    isVerbose: true,
     defaultTimeoutInterval: 30000
   }
 };
